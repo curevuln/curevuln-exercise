0435c138a2f67d749d80572da5cfce6c56ab073d
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-10-18 16:29:55 +0900

    [ rm ]不要なファイルの削除し忘れ

--- php-sessionFixation/README.md
+++
@@ -1,3 +0,0 @@
-# PHP Refrected XSS
-- guest:guest_password
-- admin:Adm1n_paSsw0Rd
--- php-sessionFixation/www/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-function connectDB() {
-    $dbname = 'mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=chat;charset=utf8mb4';
-    return new PDO($dbname, 'root', '');
-}
-?>
