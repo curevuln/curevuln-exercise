71e77ffdcecd859075a925e561b52da0b21d4c88
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2018-10-18 16:27:50 +0900

    [ rem ]削除反映

--- php-auth/README.md
+++
@@ -1,3 +0,0 @@
-# PHP アクセス制御
-- guest:guest_password
-- admin:Adm1n_paSsw0Rd
--- php-auth/www/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header("X-XSS-Protection: 0;");
-
-function connectDB() {
-    $dbname = 'mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=info;charset=utf8mb4';
-    return new PDO($dbname, 'root', '');
-}
-?>
