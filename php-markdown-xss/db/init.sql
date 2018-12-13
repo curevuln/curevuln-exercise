b73ecbb04144f8fdd57ccdc4024780c3e1e360f3
mrtc0 <mrtc0@ssrf.in>
2018-12-13 15:33:11 +0900

    テストデータを変更

--- php-markdown-xss/db/init.sql
+++ php-markdown-xss/db/init.sql
@@ -8,15 +8,7 @@ CREATE TABLE article (
     PRIMARY KEY (id)
 );
 
-INSERT INTO article (`id`, `title`, `content`) VALUES (1, 'SSRFデビュー', '<h1>こんにちはこんにちは</h1>');
-INSERT INTO article (`id`, `title`, `content`) VALUES (2, '今日は僕の誕生日です！', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (3, 'みなさんはどう思いますか？', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (4, 'あけましておめでとう！！', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (5, 'SSRFの皆さん！お知恵拝借！', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (6, 'SSRFプレミアム入っちゃいました！！', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (7, '皆様にアンケートです', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (8, '最近めちゃくちゃ落ち込んでいます…', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (9, '手首切りました…', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (10, '家にある睡眠薬全部飲みました…', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (11, 'ありがとう…', '');
-INSERT INTO article (`id`, `title`, `content`) VALUES (12, '我はメシア、明日この世界を粛清する。', '');
+INSERT INTO article (`id`, `title`, `content`) VALUES (1, 'ウェイ', '<h1>こんにちはこんにちは</h1>');
+INSERT INTO article (`id`, `title`, `content`) VALUES (2, '今日は僕の誕生日です！', '<a href="#">link</a>');
+INSERT INTO article (`id`, `title`, `content`) VALUES (3, 'みなさんはどう思いますか？', 'ワイワイ');
+INSERT INTO article (`id`, `title`, `content`) VALUES (4, 'あけましておめでとう！！', 'ウェイ;');
