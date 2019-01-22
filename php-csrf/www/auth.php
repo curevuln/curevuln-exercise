d1005f94c951653f519f0c2e6909ba6f087d3d6a
mrtc0 <mrtc0@ssrf.in>
2019-01-22 14:54:16 +0900

    fix typo

--- php-csrf/www/auth.php
+++ php-csrf/www/auth.php
@@ -1,18 +1,19 @@
 <?php
+
 require_once 'setting.php';
 require_once 'common.php';
 require './authFunction.php';
-//変数定義
-$dbh        = connectDB();
-$loginId    = $_POST['loginID'];
-$password   = $_POST['password'];
-$error      = "成功しました";
-if (!varidat($loginId, $password)) {
-    $error = "パスワード、またはLoginIDが入力されていません。";
+
+$dbh = connectDB();
+$loginId = $_POST['loginID'];
+$password = $_POST['password'];
+$error = "成功しました";
+if (!validate($loginId, $password)) {
+  $error = "パスワード、またはLoginIDが入力されていません。";
 } else {
-    if (!login ($dbh, $loginId, $password) ) {
-        $error = "パスワード、またはLoginIDが間違っています。";
-    }
+  if (!login ($dbh, $loginId, $password) ) {
+    $error = "パスワード、またはLoginIDが間違っています。";
+  }
 }
 header("Location: / ");
 exit();
